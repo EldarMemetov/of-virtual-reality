758fac821b3863fb4ac03a4f866eedfdfbb67424
EldarMemetov <eldarm9347@gmail.com>
2023-08-26 18:42:45 +0300

    yes

--- js/slide.js
+++ js/slide.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
-  // Инициализация слайдера только на меньших экранах
-  if ($(window).width() < 1440) {
-    $(".conteiner-group").slick({
-      dots: true,
-      prevArrow: $(".next-left"),
-      nextArrow: $(".next-right"),
-    });
-  }
-});
+// $(document).ready(function () {
+//   // Инициализация слайдера только на меньших экранах
+//   if ($(window).width() < 1440) {
+//     $(".conteiner-group").slick({
+//       dots: true,
+//       prevArrow: $(".next-left"),
+//       nextArrow: $(".next-right"),
+//     });
+//   }
+// });
