ca64f4793778d32830cbac9848e8b14ea90e54c4
EldarMemetov <eldarm9347@gmail.com>
2023-09-12 21:05:39 +0300

    yes

---
+++ js/slide.js
@@ -0,0 +1,66 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const nameInput = document.getElementById("name-your");
+//   const emailInput = document.getElementById("email-main");
+//   const phoneInput = document.getElementById("number-you");
+//   const lastNameInput = document.getElementById("last-name");
+//   const subjectInput = document.getElementById("text-subject");
+//   const commentInput = document.getElementById("user-comment");
+
+//   nameInput.addEventListener("input", function () {
+//     const enteredName = nameInput.value;
+//     console.log("Entered name:", enteredName);
+//     // Дополнительные операции с именем
+//   });
+
+//   emailInput.addEventListener("input", function () {
+//     const enteredEmail = emailInput.value;
+//     console.log("Entered email:", enteredEmail);
+//     // Дополнительные операции с email
+//   });
+
+//   phoneInput.addEventListener("input", function () {
+//     const enteredPhoneNumber = phoneInput.value;
+//     console.log("Entered phone:", enteredPhoneNumber);
+//     // Дополнительные операции с телефоном
+//   });
+
+//   lastNameInput.addEventListener("input", function () {
+//     const enteredLastName = lastNameInput.value;
+//     console.log("Entered last name:", enteredLastName);
+//     // Дополнительные операции с фамилией
+//   });
+
+//   subjectInput.addEventListener("input", function () {
+//     const enteredSubject = subjectInput.value;
+//     console.log("Entered subject:", enteredSubject);
+//     // Дополнительные операции с темой
+//   });
+
+//   commentInput.addEventListener("input", function () {
+//     const enteredComment = commentInput.value;
+//     console.log("Entered comment:", enteredComment);
+//     // Дополнительные операции с комментарием
+//   });
+
+//   const submitButton = document.getElementById("submit-button");
+
+//   submitButton.addEventListener("click", function () {
+//     const enteredName = nameInput.value;
+//     const enteredEmail = emailInput.value;
+//     const enteredPhoneNumber = phoneInput.value;
+//     const enteredLastName = lastNameInput.value;
+//     const enteredSubject = subjectInput.value;
+//     const enteredComment = commentInput.value;
+
+//     console.log("Entered name:", enteredName);
+//     console.log("Entered email:", enteredEmail);
+//     console.log("Entered phone:", enteredPhoneNumber);
+//     console.log("Entered last name:", enteredLastName);
+//     console.log("Entered subject:", enteredSubject);
+//     console.log("Entered comment:", enteredComment);
+
+//     // Выполните здесь дополнительные операции с введенными данными
+
+//     // Например, можно отправить данные на сервер, обновить страницу и так далее
+//   });
+// });
